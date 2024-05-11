7e71e9630987ee2b70b6c0c0c8be29dc01645262
Ali Yassin <30940865+Ali13Yassin@users.noreply.github.com>
2024-05-11 17:34:16 +0300

    Created main GUI with navigation

--- Features/GUI.py
+++ Features/GUI.py
@@ -6,8 +6,6 @@ import os
 from pathlib import Path
 import threading
 
-from ftp_server_test import *
-
 os.chdir(Path(__file__).parent) #Changes cmd directory to the one that has the py file
 
 #Checks if the required python files exist
@@ -27,54 +25,70 @@ def start():
 
 #Defines properties of the main window
 def window():
-    global wind,div,fnt #Defines the main window, and div
+    global wind,div,fnt #Defines the main window, and div and font as global variables
     wind = Tk()
-    centx = int((wind.winfo_screenwidth() - 600) / 2) #Gets coordinates of where to center the window on x-axis
-    centy = int((wind.winfo_screenheight() - 400) / 2) #Gets coordinates of where to center the window on y-axis
-    wind.geometry("600x400+{}+{}".format(centx, centy)) #Centers the window
-    wind.grid_rowconfigure(1, weight=1)     #Helps center objects
-    wind.grid_columnconfigure(1, weight=1)  #Helps center objects
+    wind.iconbitmap("dog2.ico")  # Set the icon
+    centx = int((wind.winfo_screenwidth() - 800) / 2) #Gets coordinates of where to center the window on x-axis
+    centy = int((wind.winfo_screenheight() - 500) / 2) #Gets coordinates of where to center the window on y-axis
+    wind.geometry("800x500+{}+{}".format(centx, centy)) #Centers the window
     wind.title("SMP project")
     fnt = "Manrope" #To be able to change font through one change
     div = Frame(wind) #Funny name from html that's not gonna cause issues at all lolol
     div.pack() #The frame keeps the layout decent
 
 def mainmenu():
-    clear() #To clear last menu when coming back
+    global main_frame #To clear last menu when coming back
     port_num = StringVar()
-    
-    frame = Frame(wind)
-    frame.pack(fill='both', expand=True)
+    DivisionFrame = Frame(wind)
+    DivisionFrame.pack(fill='both', expand=True)
     # Configure the grid
-    frame.grid_rowconfigure(0, weight=2)  # 20% of the space goes here
-    frame.grid_rowconfigure(1, weight=8)  # 80% of the space goes here
-    style = ttk.Style()
-    style.configure("Custom.TFrame", background="red")
-    frame1 = Frame(frame, style="Custom.TFrame")
-    frame1.grid(row=0, column=0, sticky="nsew")
-    frame2 = Frame(frame, style="Custom.TFrame")
-    frame2.grid(row=1, column=0, sticky="nsew")
-    # Label(frame, text="Welcome to Ethel project\nThis is the main menu\nUnfinished version", font=(fnt, 15)).grid(row=0, column=0, columnspan=2, sticky="nsew")
-    # Entry(frame, textvariable=port_num).grid(row=1, column=0, padx=10, pady=10)
-    Button(frame, text="Start server", command=lambda: start_listen(int(port_num.get()))).grid(row=1, column=0, padx=10, pady=10)
-    Button(frame, text="Courses actions", command=placeholder).grid(row=2, column=1, padx=10, pady=10)
+    DivisionFrame.grid_rowconfigure(0, weight=0)  # 20% of the space goes here
+    DivisionFrame.grid_rowconfigure(1, weight=10)  # 80% of the space goes here
+    DivisionFrame.grid_columnconfigure(0, weight=1)
+    style = Style()
+    style.configure("navbar.TFrame", background="#391D0D")
+    style.configure("Custom2.TFrame", background="#6F3F29")
+    style.configure("Title.TLabel", background="#391D0D", foreground="white", font=(fnt, 15, "bold"))
+    style.configure("navbutton.TLabel", background="#A5622F", foreground="white", font=(fnt, 12))  # Change the font of the text
+    style.map("navbutton.TLabel",foreground=[('pressed', '#391D0D'), ('active', 'white')],background=[('pressed', '!disabled', '#FFE7D4'), ('active', '#E99A5D')])
+    navbar = Frame(DivisionFrame, style="navbar.TFrame")
+    navbar.grid(row=0, column=0, sticky="nsew")
+    main_frame = Frame(DivisionFrame, style="Custom2.TFrame")
+    main_frame.grid(row=1, column=0, sticky="nsew")
+    Label(navbar, text="The Ethel Project", style="Title.TLabel").grid(row=0, column=0)
+    Entry(main_frame, textvariable=port_num).grid(row=0, column=0, padx=10, pady=10)
+    Button(navbar, text="FTP server", command=server_menu, style="navbutton.TLabel").grid(row=0, column=1, padx=10, pady=10)
+    Button(navbar, text="FTP client", command=client_menu, style="navbutton.TLabel").grid(row=0, column=2, padx=10, pady=10)
+    Button(navbar, text="Easy transfer", command=easy_menu, style="navbutton.TLabel").grid(row=0, column=3, padx=10, pady=10)
+    Button(navbar, text="Settings", command=settings_menu, style="navbutton.TLabel").grid(row=0, column=4, padx=10, pady=10)
 
-def start_listen(port):
-    arg = [port] #Needs to be a list to be able to pass arguments to the thread
-    t1 = threading.Thread(target=start_listening, args=arg)
-    t1.start()
 
 #------------------------------------<Initialization End>-------------------------------------------------------
+#------------------------------------<Menus Start>-------------------------------------------------------
+def server_menu():
+    clear() #Clears the previous menu
+    Button(main_frame, text="server menu test", command=placeholder, style="navbutton.TLabel").grid(row=0, column=4, padx=10, pady=10)
+
+def client_menu():
+    clear() #Clears the previous menu
+    Button(main_frame, text="client menu test", command=placeholder, style="navbutton.TLabel").grid(row=0, column=4, padx=10, pady=10)
+
+def easy_menu():
+    clear() #Clears the previous menu
+    Button(main_frame, text="easy menu test", command=placeholder, style="navbutton.TLabel").grid(row=0, column=4, padx=10, pady=10)
+
+def settings_menu():
+    clear() #Clears the previous menu
+    Button(main_frame, text="settings menu test", command=placeholder, style="navbutton.TLabel").grid(row=0, column=4, padx=10, pady=10)
+#------------------------------------<Menus End>-------------------------------------------------------
 #------------------------------------<General Start>-------------------------------------------------------
 #Used as a placeholder for menus I didn't add
 def placeholder():
-    clear() #To clear last menu
-    Label(div, text="Placeholder menu").grid(row=0, column=0, padx=10, pady=10)
-    Button(div, text="Main menu",command=mainmenu).grid(row=2, column=0, padx=10, pady=10)
+    print("Button pressed")
 
 #Deletes the widgets from previous menu and cleares memory from them
 def clear():
-    for widget in div.winfo_children():
+    for widget in main_frame.winfo_children():
         widget.destroy() #Used this instead of forget to clear memory
 
 #------------------------------------<General End>-------------------------------------------------------
