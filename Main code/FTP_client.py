6269a8f438597a0fbf0bb351935f945640f8a174
Ali Yassin <30940865+Ali13Yassin@users.noreply.github.com>
2024-05-12 05:25:58 +0300

    Client GUI and code

--- Main code/FTP_client.py
+++ Main code/FTP_client.py
@@ -8,25 +8,19 @@ os.chdir(Path(__file__).parent)
 def connect_to_server(host, username, password):
     global ftp_server
     ftp_server = ftplib.FTP(host, username, password)
-    print("Connected to " + host)
-    print(ftp_server.getwelcome())
     return ftp_server
 
 # List the files in the current directory
 def list_files():
-    print("Files in the current directory:")
-    files = ftp_server.dir()
-    print(ftp_server.nlst())
+    files = ftp_server.nlst()
     return files
 
 # Change the current directory
 def change_directory(directory):
-    print("Changing to directory: ")
     ftp_server.cwd(directory)
 
 # Create a new directory
 def create_directory(directory_name):
-    print("Creating directory: ")
     ftp_server.mkd(directory_name)
 
 # Download a file from the FTP server
@@ -36,7 +30,6 @@ def download_file(file):
 
 # Upload a file to the FTP server
 def upload_file(file):
-    print("Uploading file: ")
     with open(file, "rb") as f:
         ftp_server.storbinary("STOR " + file, f)
 
@@ -44,10 +37,11 @@ def upload_file(file):
 def close_connection():
     ftp_server.quit()
 
-connect_to_server("ftp.dlptest.com", "dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu")
-print(ftp_server.nlst())
-files = ftp_server.dir()
-change_directory(ftp_server.nlst()[0])
+# connect_to_server("ftp.dlptest.com", "dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu")
+# print(ftp_server.nlst())
+# upload_file("dog.jpg")
+# print(ftp_server.nlst())
+# change_directory(ftp_server.nlst()[0])
 
-print(ftp_server.nlst())
-print(ftp_server.pwd())
+# print(ftp_server.nlst())
+# print(ftp_server.pwd())
--- Main code/GUI.py
+++ Main code/GUI.py
@@ -6,6 +6,7 @@ import os
 from pathlib import Path
 import threading
 from PIL import Image, ImageTk
+import FTP_client as clienter
 
 os.chdir(Path(__file__).parent) #Changes cmd directory to the one that has the py file
 
@@ -59,10 +60,10 @@ def mainmenu():
     Label(navbar, text="The Ethel Project", style="Title.TLabel").grid(row=0, column=0)
     Entry(main_frame, textvariable=port_num).grid(row=0, column=0, padx=10, pady=10)
     Button(navbar, text="FTP server", command=server_menu, style="navbutton.TLabel").grid(row=0, column=1, padx=10, pady=10)
-    Button(navbar, text="FTP client", command=client_menu, style="navbutton.TLabel").grid(row=0, column=2, padx=10, pady=10)
+    Button(navbar, text="FTP client", command=client_connect_menu, style="navbutton.TLabel").grid(row=0, column=2, padx=10, pady=10)
     Button(navbar, text="Easy transfer", command=easy_menu, style="navbutton.TLabel").grid(row=0, column=3, padx=10, pady=10)
     Button(navbar, text="Settings", command=settings_menu, style="navbutton.TLabel").grid(row=0, column=4, padx=10, pady=10)
-
+    server_menu() #Starts the server menu
 
 #------------------------------------<Initialization End>-------------------------------------------------------
 #------------------------------------<Menus Start>-------------------------------------------------------
@@ -116,38 +117,137 @@ def server_menu():
     style.map("server_off_button.TLabel",foreground=[('pressed', '#391D0D'), ('active', 'white')],background=[('pressed', '!disabled', '#FFE7D4'), ('active', '#E99A5D')])
     style.configure("server_on_button.TLabel", background="#C43333", foreground="white", font=(fnt, 20, "bold"), padding=(10, 10))  # Change the font of the text
     style.map("server_on_button.TLabel",foreground=[('pressed', 'black'), ('active', 'white')],background=[('pressed', '!disabled', '#F67171'), ('active', '#E80000')])
-    Button(control_frame, text="Start server", command=placeholder, style="server_on_button.TLabel").pack(expand=True)
+    Button(control_frame, text="Start server", command=placeholder, style="server_off_button.TLabel").pack(expand=True)
 
-def client_menu():
+def client_connect_menu():
     clear() #Clears the previous menu
     client_back = Frame(main_frame, style="Custom2.TFrame")
     client_back.pack(fill='both', expand=True)
     client_back.grid_columnconfigure(0, weight=1)
-    client_back.grid_columnconfigure(1, weight=1)
     client_back.grid_rowconfigure(0, weight=1)
 
     #Controlling the server
-    control_frame = Frame(client_back, style="Custom2.TFrame")
-    control_frame.grid(row=0, column=0, sticky='nsew')
-
-
-    addr_frame = Frame(control_frame, style="Custom2.TFrame")
-    addr_frame.grid(row=0, column=0)
-    uname_frame = Frame(control_frame, style="Custom2.TFrame")
-    uname_frame.grid(row=1, column=0)
-    pass_frame = Frame(control_frame, style="Custom2.TFrame")
-    pass_frame.grid(row=2, column=0)
-    Label(addr_frame, text="Server address:", style="settings.TLabel").grid(row=0, column=0)
-    Entry(addr_frame, width=20).grid(row=0, column=1)
-    Label(uname_frame, text="Username:", style="settings.TLabel").grid(row=0, column=0)
-    Entry(uname_frame, width=20).grid(row=0, column=1)
-    Label(pass_frame, text="Password:", style="settings.TLabel").grid(row=0, column=0)
-    Entry(pass_frame, width=20).grid(row=0, column=1)
-    style.configure("server_off_button.TLabel", background="#A5622F", foreground="white", font=(fnt, 20, "bold"), padding=(10, 10))  # Change the font of the text
-    style.map("server_off_button.TLabel",foreground=[('pressed', '#391D0D'), ('active', 'white')],background=[('pressed', '!disabled', '#FFE7D4'), ('active', '#E99A5D')])
-    Button(control_frame, text="Connect to server", command=placeholder, style="server_off_button.TLabel").grid(row=3, column=0, pady=10)
-
+    # security_frame = Frame(options_frame, style="navbar.TFrame")
+    # security_frame.pack(padx=10, pady=10)
+    control_frame = Frame(client_back, style="navbar.TFrame")
+    control_frame.grid(row=0, column=0)
     
+
+    addr_frame = Frame(control_frame, style="navbar.TFrame")
+    addr_frame.grid(row=0, column=0)
+    uname_frame = Frame(control_frame, style="navbar.TFrame")
+    uname_frame.grid(row=1, column=0)
+    pass_frame = Frame(control_frame, style="navbar.TFrame")
+    pass_frame.grid(row=2, column=0)
+
+    address = StringVar()
+    username = StringVar()
+    password = StringVar()
+
+    style.configure("settings.TLabel", background="#391D0D", foreground="white", font=(fnt, 15))
+    style.configure("darkbrown.TEntry", background="#391D0D", foreground="black", font=(fnt, 15))
+    Label(addr_frame, text="Server address:", style="settings.TLabel").grid(row=0, column=0)
+    Entry(addr_frame, textvariable=address, width=20, style='darkbrown.TEntry').grid(row=0, column=1)
+    Label(uname_frame, text="Username:", style="settings.TLabel").grid(row=0, column=0)
+    Entry(uname_frame, textvariable=username, width=20, style='darkbrown.TEntry').grid(row=0, column=1)
+    Label(pass_frame, text="Password:", style="settings.TLabel").grid(row=0, column=0)
+    Entry(pass_frame, textvariable=password, width=20, style='darkbrown.TEntry').grid(row=0, column=1)
+    style.configure("server_off_button.TLabel", background="#A5622F", foreground="white", font=(fnt, 15, "bold"))  # Change the font of the text
+    style.map("server_off_button.TLabel",foreground=[('pressed', '#391D0D'), ('active', 'white')],background=[('pressed', '!disabled', '#FFE7D4'), ('active', '#E99A5D')])
+    Button(control_frame, text="Connect to server", command=lambda: clienter_start(address.get(),username.get(),password.get()), style="server_off_button.TLabel").grid(row=3, column=0, pady=10)
+
+def client_connected_menu():
+    clear() #Clears the previous menu
+    client_back = Frame(main_frame, style="Custom2.TFrame")
+    client_back.pack(fill='both', expand=True)
+    client_back.grid_columnconfigure(0, weight=1)
+    client_back.grid_rowconfigure(0, weight=1)
+
+    info_frame = Frame(client_back, style="navbar.TFrame")
+    info_frame.grid(row=0, column=0)
+    # info_frame.pack_propagate(0)
+    Label(info_frame, text="Connection information", style="Title.TLabel").grid(row=0, column=0)
+    Label(info_frame, text="Address: ", style="settings.TLabel").grid(row=1, column=0)
+    adress_info = Label(info_frame, text="ip adress", style="settings.TLabel")
+    adress_info.grid(row=1, column=1)
+    Label(info_frame, text="Connection status: ", style="settings.TLabel").grid(row=2, column=0)
+    connection_info = Label(info_frame, text="Connected", style="settings.TLabel")
+    connection_info.grid(row=2, column=1)
+
+    action_frame = Frame(client_back, style="navbar.TFrame")
+    action_frame.grid(row=1, column=0)
+    Label(action_frame, text="Actions", style="Title.TLabel").grid(row=0, column=0)
+    Button(action_frame, text="View all files", command=lambda: view_files_menu(0), style="navbutton.TLabel").grid(row=1, column=0, padx=10, pady=10)
+    Button(action_frame, text="Download file", command=lambda: view_files_menu(1), style="navbutton.TLabel").grid(row=2, column=0, padx=10, pady=10)
+    Button(action_frame, text="Upload file", command=lambda: view_files_menu(2), style="navbutton.TLabel").grid(row=3, column=0, padx=10, pady=10)
+    
+    style.configure("server_on_button.TLabel", background="#C43333", foreground="white", font=(fnt, 15))  # Change the font of the text
+    style.map("server_on_button.TLabel",foreground=[('pressed', 'black'), ('active', 'white')],background=[('pressed', '!disabled', '#F67171'), ('active', '#E80000')])
+    Button(action_frame, text="Disconnect", command=lambda: clienter.close_connection(), style="server_on_button.TLabel").grid(row=4, column=0, padx=10, pady=10)
+
+def view_files_menu(action):
+    new_window = Toplevel(wind)
+    new_window.title("Server files")
+    back_frame = Frame(new_window, style="navbar.TFrame")
+    back_frame.pack(fill='both', expand=True)
+    # Create a Treeview widget
+    tree = Treeview(back_frame)
+
+    # Define columns
+    tree["columns"] = ("Name", "Size", "Type")
+
+    # Format columns
+    tree.column("#0", width=0, stretch=NO)
+    tree.column("Name", anchor=W, width=200)
+    tree.column("Size", anchor=W, width=100)
+    tree.column("Type", anchor=W, width=100)
+
+    # Create headings
+    tree.heading("#0", text="", anchor=W)
+    tree.heading("Name", text="Name", anchor=W)
+    tree.heading("Size", text="Size", anchor=W)
+    tree.heading("Type", text="Type", anchor=W)
+
+    # Add data to the treeview
+    for i in clienter.list_files():
+        split = i.split(".")
+        split = split[len(split)-1]
+        tree.insert("", "end", text=i, values=(i, "10 KB", split))
+
+    # Display the treeview
+    tree.pack(fill=BOTH, expand=True)
+    if action == 1:
+        def download():
+            selected_items = tree.selection()
+            for item in selected_items:
+                clienter.download_file(tree.item(item)["values"][0])
+        Button(back_frame, text="Download", command=download, style="navbutton.TLabel").pack(pady=10)
+
+    elif action == 2:
+        tree = Treeview(new_window, columns=("File Name", "Size", "Type"), selectmode='browse')
+        def upload():
+            from tkinter.filedialog import askopenfilename
+            filename = askopenfilename()
+            if filename:
+                print(filename)
+                clienter.upload_file(filename)
+                new_window.destroy()
+        def new_fold():
+            new_fold_name = StringVar()
+            Entry_frame = Frame(back_frame)
+            Entry_frame.pack(pady=5)
+            def confirm():
+                if selected_items != ():
+                    clienter.change_directory(tree.item(selected_items[0])["values"][0])
+                clienter.create_directory(new_fold_name.get())
+                new_window.destroy()
+            Label(Entry_frame, text="New folder name:").grid(row=0, column=0)
+            Entry(Entry_frame, textvariable=new_fold_name).grid(row=0, column=1)
+            Button(Entry_frame, text="Create", command=confirm).grid(row=0, column=2)
+            selected_items = tree.selection()
+        Button(back_frame, text="Upload", command=upload, style="navbutton.TLabel").pack(pady=5)
+        Button(back_frame, text="Create new folder", command=new_fold, style="navbutton.TLabel").pack(pady=5)
+
 def easy_menu():
     clear() #Clears the previous menu
     Button(main_frame, text="easy menu test", command=placeholder, style="navbutton.TLabel").grid(row=0, column=4, padx=10, pady=10)
@@ -181,7 +281,8 @@ def settings_menu():
     image_git = Label(Repo_frame, image=img2, style="desc_repo.TLabel")
     image_git.image = img2
     image_git.grid(row=0, column=0)
-    Button(Repo_frame, text="See GitHub repo", style="desc_repo.TLabel").grid(row=0, column=1)
+    import webbrowser
+    Button(Repo_frame, text="See GitHub repo", style="desc_repo.TLabel",command=lambda: webbrowser.open("https://github.com/Ali13Yassin/The-Ethel-Project")).grid(row=0, column=1)
     Repo_frame.pack(padx=10, pady=10)
 
     Button(options_frame.interior, text="settings options test", command=placeholder, style="navbutton.TLabel").pack(padx=10, pady=10)
@@ -231,6 +332,14 @@ class VerticalScrolledFrame(Frame):
 
 
 #------------------------------------<Menus End>-------------------------------------------------------
+
+def clienter_start(adr,usr,pwd):
+    clienter.connect_to_server(adr,usr,pwd)
+    if clienter.list_files() != []:
+        client_connected_menu()
+    #TODO: use threading to add a loop that checks if the connection is still active
+    else:
+        pass
 #------------------------------------<General Start>-------------------------------------------------------
 #Used as a placeholder for menus I didn't add
 def placeholder():
